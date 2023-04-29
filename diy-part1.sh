fc7ebffea28b07ebd62acf2b9acadd97891eeff0
incentia <78892324+incentia@users.noreply.github.com>
2023-04-29 11:18:39 +0800

    Update diy-part1.sh
    update passworld feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,5 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
